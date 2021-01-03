dc0380317cebf66861b59d1a4a7d4e42028839de
piltom <22397970+piltom@users.noreply.github.com>
2021-01-03 12:22:22 +0100

    corrected ORB datasets

--- create_widget_dataset.py
+++ create_widget_dataset.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 from skimage import io
-from skimage.feature import BRIEF, corner_harris, corner_peaks
+from skimage.feature import ORB
 import os
 
-descriptor_extractor = BRIEF()
+descriptor_extractor = ORB(n_keypoints=200)
 dataset_df = pd.DataFrame()
 
 folders = ['img/checkbox','img/pushbutton', 'img/input','img/radiobutton', 'img/roundimage', 'img/slider', 'img/sqimage', 'img/switch', 'img/text']
@@ -13,10 +13,10 @@ for folder in folders:
     for dirname, _, filenames in os.walk(folder):
         for filename in filenames:
             img1=io.imread(os.path.join(dirname, filename))
-            keypoints1 = corner_peaks(corner_harris(img1), min_distance=7, threshold_rel=0.10)
-            descriptor_extractor.extract(img1, keypoints1)
+            descriptor_extractor.detect_and_extract(img1)
+            keypoints1 = descriptor_extractor.keypoints
             descriptors1 = descriptor_extractor.descriptors
             descriptors_flat=np.array(descriptors1).flatten()
             dataset_df=dataset_df.append(pd.DataFrame([[filename, target_name, *descriptors_flat]]), ignore_index=True)
 dataset_df.rename({0:"filename",1:"target"},axis='columns',inplace=True)
-dataset_df.to_pickle("dataset_img_BRIEF_harris_dist7_th0-1.pkl")
+dataset_df.to_pickle("dataset_img_ORB_200.pkl")
